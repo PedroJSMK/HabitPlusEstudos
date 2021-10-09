bcb4dd5979915fd78f19641e859f870753c55252
Pedro Marques <pedrokanagusto@hotmail.com>
2021-10-09 11:01:56 -0300

    Initial Commit

---
+++ HabitPlusEstudos/HabitPlusEstudosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusEstudosApp.swift
+//  HabitPlusEstudos
+//
+//  Created by Pedro Kanagusto on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusEstudosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
